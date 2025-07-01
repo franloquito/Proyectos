e6226b6798964ec2578e302b9739e7f66fe502e1
franloquito <franciscojosemontruchit2@gmail.com>
2025-07-01 16:51:23 -0300

    Add files via upload

---
+++ HOLAMUNDO/hola_mundo.cpp
@@ -0,0 +1,6 @@
+#include <iostream>  // Biblioteca estándar de "entrada" y "salida" de C++.
+using namespace std; // Permite las cadenas de texto.
+int main() { // Aquí empieza el programa.
+    cout << "¡Hola, mundo!" << endl; // "cout" muestra el mensaje ¡Hola, mundo!
+    return 0; // Devuelve toda la información.
+}
